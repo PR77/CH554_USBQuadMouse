02ebf6dc17adf41eb861074ee0d2588bc53a7f3d
PR77 <6563857+PR77@users.noreply.github.com>
2025-03-17 17:22:37 +1100

    Updates

--- src/main.c
+++ src/main.c
@@ -20,27 +20,26 @@
 #include "serial.h"
 #include "bootloader.h"
 
-#define LED_FLASH_RATE_MS               300
-#define USB_TRANSFER_RATE_MS            8
+#define LED_FLASH_RATE_MS           300
+#define USB_TRANSFER_RATE_MS        8
 
-__code uint8_t  SetupGetDevDescr[] = { USB_REQ_TYP_IN, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_DEVICE, 0x00, 0x00, sizeof(USB_DEV_DESCR), 0x00 };
-__code uint8_t  SetupGetCfgDescr[] = { USB_REQ_TYP_IN, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_CONFIG, 0x00, 0x00, 0x04, 0x00 };
-__code uint8_t  SetupSetUsbAddr[] = { USB_REQ_TYP_OUT, USB_SET_ADDRESS, USB_DEVICE_ADDR, 0x00, 0x00, 0x00, 0x00, 0x00 };
-__code uint8_t  SetupSetUsbConfig[] = { USB_REQ_TYP_OUT, USB_SET_CONFIGURATION, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-__code uint8_t  SetupSetUsbInterface[] = { USB_REQ_RECIP_INTERF, USB_SET_INTERFACE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-__code uint8_t  SetupClrEndpStall[] = { USB_REQ_TYP_OUT | USB_REQ_RECIP_ENDP, USB_CLEAR_FEATURE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-__code uint8_t  SetupSetHIDIdle[]= { 0x21, HID_SET_IDLE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-__code uint8_t  SetupGetHIDDevReport[] = { 0x81, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_REPORT, 0x00, 0x00, 0xFF, 0x00 };
+__code uint8_t SetupGetDevDescr[] = { USB_REQ_TYP_IN, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_DEVICE, 0x00, 0x00, sizeof(USB_DEV_DESCR), 0x00 };
+__code uint8_t SetupGetCfgDescr[] = { USB_REQ_TYP_IN, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_CONFIG, 0x00, 0x00, 0x04, 0x00 };
+__code uint8_t SetupSetUsbAddr[] = { USB_REQ_TYP_OUT, USB_SET_ADDRESS, USB_DEVICE_ADDR, 0x00, 0x00, 0x00, 0x00, 0x00 };
+__code uint8_t SetupSetUsbConfig[] = { USB_REQ_TYP_OUT, USB_SET_CONFIGURATION, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+__code uint8_t SetupSetUsbInterface[] = { USB_REQ_RECIP_INTERF, USB_SET_INTERFACE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+__code uint8_t SetupClrEndpStall[] = { USB_REQ_TYP_OUT | USB_REQ_RECIP_ENDP, USB_CLEAR_FEATURE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+__code uint8_t SetupSetHIDIdle[]= { 0x21, HID_SET_IDLE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+__code uint8_t SetupGetHIDDevReport[] = { 0x81, USB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_REPORT, 0x00, 0x00, 0xFF, 0x00 };
+__code uint8_t SetupGetHubDescr[] = { HUB_GET_HUB_DESCRIPTOR, HUB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_HUB, 0x00, 0x00, sizeof( USB_HUB_DESCR ), 0x00 };
 
-__xdata uint8_t  UsbDevEndp0Size;                                                      
-__xdata __at (0x0000) uint8_t  RxBuffer[ MAX_PACKET_SIZE ];     // IN, must even address
-__xdata __at (0x0040) uint8_t  TxBuffer[ MAX_PACKET_SIZE ];     // OUT, must even address
-
-__code uint8_t  SetupGetHubDescr[] = { HUB_GET_HUB_DESCRIPTOR, HUB_GET_DESCRIPTOR, 0x00, USB_DESCR_TYP_HUB, 0x00, 0x00, sizeof( USB_HUB_DESCR ), 0x00 };
+__xdata uint8_t UsbDevEndp0Size;                                                      
+__xdata __at (0x0000) uint8_t RxBuffer[ MAX_PACKET_SIZE ];  // IN, must even address
+__xdata __at (0x0040) uint8_t TxBuffer[ MAX_PACKET_SIZE ];  // OUT, must even address
 
 uint8_t Set_Port = 0;
-__xdata _RootHubDev ThisUsbDev;                                 //ROOT
-__xdata _DevOnHubPort DevOnHubPort[HUB_MAX_PORTS];              // assumption: no more than 1 external HUB, each external HUB does not exceed HUB_MAX_PORTS ports (don’t care if there are more)
+__xdata _RootHubDev ThisUsbDev;                             // ROOT
+__xdata _DevOnHubPort DevOnHubPort[HUB_MAX_PORTS];          // Assumption: no more than 1 external HUB, each external HUB does not exceed HUB_MAX_PORTS ports (don’t care if there are more)
 __bit FoundNewDev;
 
 void main(void) {
@@ -129,20 +128,20 @@ void main(void) {
 
             usbLocation = SearchTypeDevice(DEV_TYPE_MOUSE);
 
-            if(usbLocation != 0xFFFF) {                                   // found a mouse (how to deal with two mice?)
+            if(usbLocation != 0xFFFF) {                                     // found a mouse (how to deal with two mice?)
                 ssd1306_setCursor(0, 2);
                 ssd1306_printString("ENUMERATION LOC ");
                 ssd1306_printHexWord(usbLocation);
             
                 ssd1306_setCursor(0, 3);
                 SelectHubPort(usbLocation);                                 // select to operate designated ROOT-HUB port, set current USB speed and USB address of operated device
-                usbLocation = usbLocation >> 8;                                 // CH554 has only one USB, only the lower eight bits are required
+                usbLocation = usbLocation >> 8;                             // CH554 has only one USB, only the lower eight bits are required
                 endp = usbLocation ? DevOnHubPort[usbLocation-1].GpVar[0] : ThisUsbDev.GpVar[0];  // address of interrupt endpoint, bit 7 is used for synchronization flag bit
                 
-                if(endp & USB_ENDP_ADDR_MASK) {                     // endpoint valid
+                if(endp & USB_ENDP_ADDR_MASK) {                             // endpoint valid
                     usbStatus = USBHostTransact(USB_PID_IN << 4 | endp & 0x7F, endp & 0x80 ? bUH_R_TOG | bUH_T_TOG : 0, 0); // CH554 transmit transaction, get data, NAK does not retry
                     if(usbStatus == ERR_SUCCESS) {
-                        endp ^= 0x80;                                   // flip sync flag
+                        endp ^= 0x80;                                       // flip sync flag
 
                         if(usbLocation) {
                             DevOnHubPort[usbLocation-1].GpVar[0] = endp;    // save synchronization flag
@@ -150,7 +149,7 @@ void main(void) {
                             ThisUsbDev.GpVar[0] = endp;
                         }
                 
-                        len = USB_RX_LEN;                               // received data length
+                        len = USB_RX_LEN;                                   // received data length
                         if(len) {
                             for(uint8_t i = 0; i < 4; i++) {
                                 // Byte 0 - Buttons
@@ -189,7 +188,7 @@ void main(void) {
                     ssd1306_printString("NO INTRPT END POINT  ");
                 }
                 
-                SetUsbSpeed(1);                                     // default is full speed
+                SetUsbSpeed(1);                                             // default is full speed
             }
         }
 
